42747e406aed919dbc03ab5b471b9004ae6c96af
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-05-18 11:22:23 +0200

    js added local position script

---  Homework w 2 and 4/src/index.js
+++  Homework w 2 and 4/src/index.js
@@ -38,11 +38,25 @@ function search(city) {
 function handleCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
-
   search(city);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleCity);
+
+function showLocalPosition(position) {
+  let apiKey = "8cd9be374c7c96c39a9fe73f4bf2f055";
+  let lon = document.querySelector(position.coords.longitude);
+  let lat = document.querySelector(position.coords.latitude);
+  let localApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(localApiUrl).then(showWeather);
+}
+
+function showLocal(event) {
+  navigator.geolocation.getCurrentPosition(showLocalPosition);
+}
+
+let localButton = document.querySelector("#local-weather");
+localButton.addEventListener("click", showLocal);
 
 search("Malaga");
